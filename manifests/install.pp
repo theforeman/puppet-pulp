5fec5b66097bee82f79a4bf0a3791eaebef8d150
Cristian Falcas <falcas@optymyze.com>
2015-07-03 14:41:38 +0300

    make pulp configurable

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,35 @@
 # Pulp Installation Packages
+# Private class
 class pulp::install {
+  package { ['pulp-server', 'pulp-selinux']: ensure => $pulp::version, }
 
-  package{ ['pulp-server', 'pulp-selinux', 'pulp-docker-plugins', 'pulp-rpm-plugins', 'pulp-puppet-plugins', 'pulp-nodes-parent']:
-    ensure => installed,
+  if $pulp::messaging_transport == 'qpid' {
+    ensure_packages(['python-gofer-qpid'], {
+      ensure => $pulp::version
+    }
+    )
   }
 
+  if $pulp::messaging_transport == 'rabbitmq' {
+    ensure_packages(['python-gofer-amqp'], {
+      ensure => $pulp::version
+    }
+    )
+  }
+
+  if $pulp::enable_rpm {
+    package { ['pulp-rpm-plugins']: ensure => $pulp::version, }
+  }
+
+  if $pulp::enable_docker {
+    package { ['pulp-docker-plugins']: ensure => $pulp::version, }
+  }
+
+  if $pulp::enable_puppet {
+    package { ['pulp-puppet-plugins']: ensure => $pulp::version, }
+  }
+
+  if $pulp::enable_python {
+    package { ['pulp-python-plugins']: ensure => $pulp::version, }
+  }
 }
