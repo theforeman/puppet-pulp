c24065e8dd7113c171d41a14180e4a55d44a57b2
Cristian Falcas <falcas@optymyze.com>
2015-07-16 23:42:50 +0300

    fix config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,82 +1,76 @@
 # Pulp Master Configuration
 # Private class
 class pulp::config {
-  if $pulp::enable_puppet {
-    exec {'selinux_pulp_manage_puppet':
-      command => 'semanage boolean -m --on pulp_manage_puppet',
-      path    => '/sbin:/usr/sbin:/bin:/usr/bin',
-      onlyif  => 'getsebool pulp_manage_puppet | grep off',
-    }
-  }
-
-  file {'/var/lib/pulp/packages':
+  file { '/var/lib/pulp/packages':
     ensure => directory,
     owner  => 'apache',
     group  => 'apache',
-    mode   => '0755';
+    mode   => '0755',
   }
 
-  file {'/etc/pulp/server.conf':
+  file { '/etc/pulp/server.conf':
     ensure  => file,
-    content => template('pulp/etc/pulp/server.conf.erb'),
+    content => template('pulp/server.conf.erb'),
     owner   => 'apache',
     group   => 'apache',
     mode    => '0600',
   }
 
-  if $pulp::enable_rpm {
-  file {'/etc/pulp/repo_auth.conf':
-    ensure  => file,
-    content => template('pulp/etc/pulp/repo_auth.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
+  file { '/etc/pki/pulp/content/pulp-global-repo.ca':
+    ensure => link,
+    target => $pulp::ca_cert,
   }
 
-  file {'/etc/pki/pulp/content/pulp-global-repo.ca':
-      ensure => link,
-      target => $pulp::ca_cert,
+  if $pulp::enable_rpm {
+    file { '/etc/pulp/repo_auth.conf':
+      ensure  => file,
+      content => template('pulp/repo_auth.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+
+    file { '/etc/pulp/server/plugins.conf.d/yum_importer.json':
+      ensure  => file,
+      content => template('pulp/yum_importer.json'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+
+    file { '/etc/pulp/server/plugins.conf.d/iso_importer.json':
+      ensure  => file,
+      content => template('pulp/iso_importer.json'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
   }
 
   if $pulp::enable_docker {
-  file {'/etc/pulp/server/plugins.conf.d/docker_importer.json':
-    ensure  => file,
-    content => template('pulp/docker_importer.json'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
-  }
-
-  if $pulp::enable_rpm {
-  file {'/etc/pulp/server/plugins.conf.d/yum_importer.json':
-    ensure  => file,
-    content => template('pulp/yum_importer.json'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
+    file { '/etc/pulp/server/plugins.conf.d/docker_importer.json':
+      ensure  => file,
+      content => template('pulp/docker_importer.json'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
   }
 
   if $pulp::enable_puppet {
-  file {'/etc/pulp/server/plugins.conf.d/puppet_importer.json':
-    ensure  => file,
-    content => template('pulp/puppet_importer.json'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
-  }
+    exec { 'selinux_pulp_manage_puppet':
+      command => 'semanage boolean -m --on pulp_manage_puppet',
+      path    => '/sbin:/usr/sbin:/bin:/usr/bin',
+      onlyif  => 'getsebool pulp_manage_puppet | grep off',
+    }
 
-  if $pulp::enable_rpm {
-  file {'/etc/pulp/server/plugins.conf.d/iso_importer.json':
-    ensure  => file,
-    content => template('pulp/iso_importer.json'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
+    file { '/etc/pulp/server/plugins.conf.d/puppet_importer.json':
+      ensure  => file,
+      content => template('pulp/puppet_importer.json'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
   }
 
   file { '/etc/default/pulp_workers':
@@ -88,13 +82,11 @@ class pulp::config {
   }
 
   if $pulp::reset_cache {
-    exec {'reset_pulp_cache':
+    exec { 'reset_pulp_cache':
       command => 'rm -rf /var/lib/pulp/packages/*',
       path    => '/sbin:/bin:/usr/bin',
       before  => Exec['migrate_pulp_db'],
-      require => [
-        File['/var/lib/pulp/packages'],
-        ],
+      require => File['/var/lib/pulp/packages'],
     }
   }
 
