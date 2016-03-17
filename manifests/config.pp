d50cc5c41e6a3c4d4bf45e31bc80f6da98d9a660
Chris Duryee <cduryee@redhat.com>
2016-03-17 10:22:38 -0400

    update to add defaultsite to squid conf
    Squid 3.1 (el6/centos6 default version) requires this directive to be set for
    the Pulp streamer. Without it, the streamer will return 400 errors.

--- manifests/config.pp
+++ manifests/config.pp
@@ -106,7 +106,7 @@ class pulp::config {
     }
     class { '::squid3':
       use_deprecated_opts           => $deprecated_opts,
-      http_port                     => [ '3128 accel' ],
+      http_port                     => [ '3128 accel defaultsite=127.0.0.1:8751' ],
       acl                           => [ 'Safe_ports port 3128' ],
       http_access                   => [ 'allow localhost', 'deny to_localhost', 'deny all' ],
       cache                         => [ 'allow all' ],
