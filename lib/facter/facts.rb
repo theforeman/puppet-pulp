bd4525d3992276c62e89918e36750ea1ccddd0d8
Chadwick Banning <walkamongus@users.noreply.github.com>
2016-08-29 14:39:52 -0400

    Mongodb fact refactor (#115)
    * redirect STDERR to STDOUT to prevent yum info error on systems without mongodb packages
    
    * check for executable existence before running command
    
    * Redirect command STDERR to /dev/null

--- lib/facter/facts.rb
+++ lib/facter/facts.rb
@@ -1,14 +1,20 @@
 Facter.add(:mongodb_version) do
   setcode do
-    commands = ["rpmquery --qf='%{version}-%{release}' mongodb",
-                "repoquery --qf='%{version}-%{release}' mongodb",
-                %[LC_ALL=en_US yum -e 0 -d 0 info mongodb | awk '/^Version/ { version=$3; } /^Release/ { print version "-" $3; exit }']]
+    ENV['LC_ALL'] = 'en_US'
+    commands = [
+      "rpmquery --qf='%{version}-%{release}' mongodb",
+      "repoquery --qf='%{version}-%{release}' mongodb",
+      %[yum -e 0 -d 0 info mongodb 2>&1 | awk '/^Version/ { version=$3; } /^Release/ { print version "-" $3; exit }']
+    ]
     ret = nil
     commands.each do |command|
-      version = `#{command}`.chomp
-      if $?.success? && !version.empty?
-        ret = version
-        break
+      exe = command.partition(' ').first
+      if system("which #{exe} > /dev/null 2>&1")
+        version = `#{command} 2> /dev/null`.chomp
+        if $?.success? && !version.empty?
+          ret = version
+          break
+        end
       end
     end
     ret
