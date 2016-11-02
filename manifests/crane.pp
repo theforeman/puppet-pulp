ce34cd4313b339f49805d02d4f86d4a568dcd4b4
Stephen Benjamin <stephen@redhat.com>
2016-11-02 14:38:25 -0400

    Address feedback

--- manifests/crane.pp
+++ manifests/crane.pp
@@ -4,31 +4,22 @@
 #
 # === Parameters:
 #
-# $key::     Path to the SSL key for https
-#
-# $cert::    Path to the SSL certificate for https
-#
-# $ca_cert:: Path to the SSL CA cert for https
-#
-# $port::    Port for Crane to run on
+# $port::     Port for Crane to run on
+#             type:integer
 #
 # $data_dir:: Directory containing docker v1/v2 artifacts published by pulp
 #
 class pulp::crane (
-  $key      = $pulp::crane::params::key,
-  $cert     = $pulp::crane::params::cert,
-  $ca_cert  = $pulp::crane::params::ca_cert,
   $port     = $pulp::crane::params::port,
   $data_dir = $pulp::crane::params::data_dir,
   ) inherits pulp::crane::params {
 
-  validate_absolute_path($key, $cert, $ca_cert, $data_dir)
+  validate_absolute_path($data_dir)
   validate_integer($port)
 
   class { '::pulp::crane::install': } ~>
-  class { '::pulp::crane::apache': } ~>
   class { '::pulp::crane::config': } ~>
-  Service['httpd'] ->
+  class { '::pulp::crane::apache': } ->
   Class['crane']
 }
 
--- manifests/crane/params.pp
+++ manifests/crane/params.pp
@@ -1,8 +1,5 @@
 # Default params for Crane
 class pulp::crane::params {
-  $fqdn = $::fqdn
   $port = 5000
-  $key = undef
-  $cert = undef
-  $ca_cert = undef
+  $data_dir = '/var/lib/pulp/published/docker/v2/app'
 }
