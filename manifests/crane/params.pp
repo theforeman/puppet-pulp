5acb4772457294683ac0ee78b4c95886764ba1ac
Stephen Benjamin <stephen@redhat.com>
2016-11-03 10:44:15 -0400

    Debug in params

--- manifests/crane/params.pp
+++ manifests/crane/params.pp
@@ -1,9 +1,10 @@
 # Default params for Crane
 class pulp::crane::params {
-  $port = 5000
+  $ca_cert  = undef
+  $cert = undef
   $data_dir = '/var/lib/crane/metadata'
   $data_dir_polling_interval = 60
-  $key      = undef
-  $cert     = undef
-  $ca_cert  = undef
+  $debug = false
+  $key = undef
+  $port = 5000
 }
