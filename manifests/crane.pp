bea0b4af69b1cc1f4f1ff5f7c602c6fd9a8f64ad
Stephen Benjamin <stephen@redhat.com>
2016-11-03 09:54:10 -0400

    Crane data dir polling

--- manifests/crane.pp
+++ manifests/crane.pp
@@ -4,27 +4,30 @@
 #
 # === Parameters:
 #
-# $key::     Path to the SSL key for https
+# $key::                        Path to the SSL key for https
 #
-# $cert::    Path to the SSL certificate for https
+# $cert::                       Path to the SSL certificate for https
 #
-# $ca_cert:: Path to the SSL CA cert for https
+# $ca_cert::                    Path to the SSL CA cert for https
 #
-# $port::     Port for Crane to run on
-#             type:integer
+# $port::                       Port for Crane to run on
+#                               type:integer
 #
-# $data_dir:: Directory containing docker v1/v2 artifacts published by pulp
+# $data_dir::                   Directory containing docker v1/v2 artifacts published by pulp
 #
+# $data_dir_polling_interval::  The number of seconds between checks for updates to metadata files in the data_dir
+#                               type:integer
 class pulp::crane (
-  $port     = $pulp::crane::params::port,
-  $data_dir = $pulp::crane::params::data_dir,
-  $key      = $pulp::crane::params::key,
-  $cert     = $pulp::crane::params::cert,
-  $ca_cert  = $pulp::crane::params::ca_cert,
+  $port                      = $pulp::crane::params::port,
+  $data_dir                  = $pulp::crane::params::data_dir,
+  $data_dir_polling_interval = $pulp::crane::params::data_dir_polling_interval,
+  $key                       = $pulp::crane::params::key,
+  $cert                      = $pulp::crane::params::cert,
+  $ca_cert                   = $pulp::crane::params::ca_cert,
   ) inherits pulp::crane::params {
 
   validate_absolute_path($data_dir, $key, $cert, $ca_cert)
-  validate_integer($port)
+  validate_integer($port, $data_dir_polling_interval)
 
   class { '::pulp::crane::install': } ~>
   class { '::pulp::crane::config': } ~>
--- manifests/crane/params.pp
+++ manifests/crane/params.pp
@@ -1,7 +1,8 @@
 # Default params for Crane
 class pulp::crane::params {
   $port = 5000
-  $data_dir = '/var/lib/pulp/published/docker/v2/app'
+  $data_dir = '/var/lib/crane/metadata'
+  $data_dir_polling_interval = 60
   $key      = undef
   $cert     = undef
   $ca_cert  = undef
