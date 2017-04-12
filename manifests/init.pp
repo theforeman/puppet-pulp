66b258c87a9df2765cd5634fbe3f14d20143752d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-04-12 16:31:07 +0200

    Convert to Puppet 4 types

--- manifests/init.pp
+++ manifests/init.pp
@@ -245,179 +245,130 @@
 #
 # $ldap_url::                   URL to use for LDAP authentication. Defaults
 #                               to undef (internal authentication is used)
-#                               type:string
 #
 # $ldap_bind_dn::               LDAP Bind DN
-#                               type:string
 #
 # $ldap_bind_password::         LDAP Password
-#                               type:string
 #
 # $ldap_remote_user_attribute:: LDAP Remote User Attribute. Defaults to 'sAMAccountName'
-#                               type:string
 #
 class pulp (
-  $version                   = $pulp::params::version,
-  $crane_debug               = $pulp::params::crane_debug,
-  $crane_port                = $pulp::params::crane_port,
-  $crane_data_dir            = $pulp::params::crane_data_dir,
-  $db_name                   = $pulp::params::db_name,
-  $db_seeds                  = $pulp::params::db_seeds,
-  $db_username               = $pulp::params::db_username,
-  $db_password               = $pulp::params::db_password,
-  $db_replica_set            = $pulp::params::db_replica_set,
-  $db_ssl                    = $pulp::params::db_ssl,
-  $db_ssl_keyfile            = $pulp::params::db_ssl_keyfile,
-  $db_ssl_certfile           = $pulp::params::db_ssl_certfile,
-  $db_verify_ssl             = $pulp::params::db_verify_ssl,
-  $db_ca_path                = $pulp::params::db_ca_path,
-  $db_unsafe_autoretry       = $pulp::params::db_unsafe_autoretry,
-  $db_write_concern          = $pulp::params::db_write_concern,
-  $server_name               = $pulp::params::server_name,
-  $key_url                   = $pulp::params::key_url,
-  $ks_url                    = $pulp::params::ks_url,
-  $default_login             = $pulp::params::default_login,
-  $default_password          = $pulp::params::default_password,
-  $debugging_mode            = $pulp::params::debugging_mode,
-  $log_level                 = $pulp::params::log_level,
-  $server_working_directory  = $pulp::params::server_working_directory,
-  $rsa_key                   = $pulp::params::rsa_key,
-  $rsa_pub                   = $pulp::params::rsa_pub,
-  $ca_cert                   = $pulp::params::ca_cert,
-  $ca_key                    = $pulp::params::ca_key,
-  $https_cert                = $pulp::params::https_cert,
-  $https_key                 = $pulp::params::https_key,
-  $https_chain               = $pulp::params::https_chain,
-  $ssl_username              = $pulp::params::ssl_username,
-  $user_cert_expiration      = $pulp::params::user_cert_expiration,
-  $consumer_cert_expiration  = $pulp::params::consumer_cert_expiration,
-  $serial_number_path        = $pulp::params::serial_number_path,
-  $consumer_history_lifetime = $pulp::params::consumer_history_lifetime,
-  $oauth_enabled             = $pulp::params::oauth_enabled,
-  $oauth_key                 = $pulp::params::oauth_key,
-  $oauth_secret              = $pulp::params::oauth_secret,
-  $max_keep_alive            = $pulp::params::max_keep_alive,
-  $messaging_url             = $pulp::params::messaging_url,
-  $messaging_transport       = $pulp::params::messaging_transport,
-  $messaging_auth_enabled    = $pulp::params::messaging_auth_enabled,
-  $messaging_ca_cert         = $pulp::params::messaging_ca_cert,
-  $messaging_client_cert     = $pulp::params::messaging_client_cert,
-  $messaging_topic_exchange  = $pulp::params::messaging_topic_exchange,
-  $messaging_event_notifications_enabled = $pulp::params::messaging_event_notifications_enabled,
-  $messaging_event_notification_url = $pulp::params::messaging_event_notification_url,
-  $messaging_version         = $pulp::params::messaging_version,
-  $broker_url                = $pulp::params::broker_url,
-  $broker_use_ssl            = $pulp::params::broker_use_ssl,
-  $tasks_login_method        = $pulp::params::tasks_login_method,
-  $email_host                = $pulp::params::email_host,
-  $email_port                = $pulp::params::email_port,
-  $email_from                = $pulp::params::email_from,
-  $email_enabled             = $pulp::params::email_enabled,
-  $manage_squid              = $pulp::params::manage_squid,
-  $lazy_redirect_host        = $pulp::params::lazy_redirect_host,
-  $lazy_redirect_port        = $pulp::params::lazy_redirect_port,
-  $lazy_redirect_path        = $pulp::params::lazy_redirect_path,
-  $lazy_https_retrieval      = $pulp::params::lazy_https_retrieval,
-  $lazy_download_interval    = $pulp::params::lazy_download_interval,
-  $lazy_download_concurrency = $pulp::params::lazy_download_concurrency,
-  $consumers_crl             = $pulp::params::consumers_crl,
-  $reset_cache               = $pulp::params::reset_cache,
-  $ssl_verify_client         = $pulp::params::ssl_verify_client,
-  $ssl_protocol              = $pulp::params::ssl_protocol,
-  $repo_auth                 = $pulp::params::repo_auth,
-  $proxy_url                 = $pulp::params::proxy_url,
-  $proxy_port                = $pulp::params::proxy_port,
-  $proxy_username            = $pulp::params::proxy_username,
-  $proxy_password            = $pulp::params::proxy_password,
-  $yum_max_speed             = $pulp::params::yum_max_speed,
-  $num_workers               = $pulp::params::num_workers,
-  $enable_katello            = $pulp::params::enable_katello,
-  $enable_crane              = $pulp::params::enable_crane,
-  $max_tasks_per_child       = $pulp::params::max_tasks_per_child,
-  $enable_docker             = $pulp::params::enable_docker,
-  $enable_rpm                = $pulp::params::enable_rpm,
-  $enable_puppet             = $pulp::params::enable_puppet,
-  $enable_python             = $pulp::params::enable_python,
-  $enable_ostree             = $pulp::params::enable_ostree,
-  $enable_parent_node        = $pulp::params::enable_parent_node,
-  $enable_http               = $pulp::params::enable_http,
-  $manage_broker             = $pulp::params::manage_broker,
-  $manage_db                 = $pulp::params::manage_db,
-  $manage_httpd              = $pulp::params::manage_httpd,
-  $manage_plugins_httpd      = $pulp::params::manage_plugins_httpd,
-  $node_certificate          = $pulp::params::node_certificate,
-  $node_verify_ssl           = $pulp::params::node_verify_ssl,
-  $node_server_ca_cert       = $pulp::params::node_server_ca_cert,
-  $node_oauth_effective_user = $pulp::params::node_oauth_effective_user,
-  $node_oauth_key            = $pulp::params::node_oauth_key,
-  $node_oauth_secret         = $pulp::params::node_oauth_secret,
-  $disabled_authenticators   = $pulp::params::disabled_authenticators,
-  $additional_wsgi_scripts   = $pulp::params::additional_wsgi_scripts,
-  $puppet_wsgi_processes     = $pulp::params::puppet_wsgi_processes,
-  $migrate_db_timeout        = $pulp::params::migrate_db_timeout,
-  $show_conf_diff            = $pulp::params::show_conf_diff,
-  $enable_profiling          = $pulp::params::enable_profiling,
-  $profiling_directory       = $pulp::params::profiling_directory,
-  $ldap_url                  = $pulp::params::ldap_url,
-  $ldap_bind_dn              = $pulp::params::ldap_bind_dn,
-  $ldap_bind_password        = $pulp::params::ldap_bind_password,
-  $ldap_remote_user_attribute = $pulp::params::ldap_remote_user_attribute,
+  String $version = $::pulp::params::version,
+  Boolean $crane_debug = $::pulp::params::crane_debug,
+  Integer[1, 65535] $crane_port = $::pulp::params::crane_port,
+  Stdlib::Absolutepath $crane_data_dir = $::pulp::params::crane_data_dir,
+  String $db_name = $::pulp::params::db_name,
+  String $db_seeds = $::pulp::params::db_seeds,
+  Optional[String] $db_username = $::pulp::params::db_username,
+  Optional[String] $db_password = $::pulp::params::db_password,
+  Optional[String] $db_replica_set = $::pulp::params::db_replica_set,
+  Boolean $db_ssl = $::pulp::params::db_ssl,
+  Optional[Stdlib::Absolutepath] $db_ssl_keyfile = $::pulp::params::db_ssl_keyfile,
+  Optional[Stdlib::Absolutepath] $db_ssl_certfile = $::pulp::params::db_ssl_certfile,
+  Boolean $db_verify_ssl = $::pulp::params::db_verify_ssl,
+  Stdlib::Absolutepath $db_ca_path = $::pulp::params::db_ca_path,
+  Boolean $db_unsafe_autoretry = $::pulp::params::db_unsafe_autoretry,
+  Optional[Enum['majority', 'all']] $db_write_concern = $::pulp::params::db_write_concern,
+  String $server_name = $::pulp::params::server_name,
+  String $key_url = $::pulp::params::key_url,
+  String $ks_url = $::pulp::params::ks_url,
+  String $default_login = $::pulp::params::default_login,
+  String $default_password = $::pulp::params::default_password,
+  Boolean $debugging_mode = $::pulp::params::debugging_mode,
+  Enum['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'] $log_level = $::pulp::params::log_level,
+  Optional[Stdlib::Absolutepath] $server_working_directory = $::pulp::params::server_working_directory,
+  Stdlib::Absolutepath $rsa_key = $::pulp::params::rsa_key,
+  Stdlib::Absolutepath $rsa_pub = $::pulp::params::rsa_pub,
+  Stdlib::Absolutepath $ca_cert = $::pulp::params::ca_cert,
+  Stdlib::Absolutepath $ca_key = $::pulp::params::ca_key,
+  Optional[Stdlib::Absolutepath] $https_cert = $::pulp::params::https_cert,
+  Optional[Stdlib::Absolutepath] $https_key = $::pulp::params::https_key,
+  Optional[Stdlib::Absolutepath] $https_chain = $::pulp::params::https_chain,
+  Variant[String, Boolean] $ssl_username = $::pulp::params::ssl_username,
+  Integer $user_cert_expiration = $::pulp::params::user_cert_expiration,
+  Integer $consumer_cert_expiration = $::pulp::params::consumer_cert_expiration,
+  Stdlib::Absolutepath $serial_number_path = $::pulp::params::serial_number_path,
+  Integer[-1] $consumer_history_lifetime = $::pulp::params::consumer_history_lifetime,
+  Boolean $oauth_enabled = $::pulp::params::oauth_enabled,
+  String $oauth_key = $::pulp::params::oauth_key,
+  String $oauth_secret = $::pulp::params::oauth_secret,
+  Integer[0] $max_keep_alive = $::pulp::params::max_keep_alive,
+  String $messaging_url = $::pulp::params::messaging_url,
+  Enum['qpid', 'rabbitmq'] $messaging_transport = $::pulp::params::messaging_transport,
+  Boolean $messaging_auth_enabled = $::pulp::params::messaging_auth_enabled,
+  Optional[Stdlib::Absolutepath] $messaging_ca_cert = $::pulp::params::messaging_ca_cert,
+  Optional[Stdlib::Absolutepath] $messaging_client_cert = $::pulp::params::messaging_client_cert,
+  String $messaging_topic_exchange = $::pulp::params::messaging_topic_exchange,
+  Boolean $messaging_event_notifications_enabled = $::pulp::params::messaging_event_notifications_enabled,
+  Optional[String] $messaging_event_notification_url = $::pulp::params::messaging_event_notification_url,
+  String $messaging_version = $::pulp::params::messaging_version,
+  String $broker_url = $::pulp::params::broker_url,
+  Boolean $broker_use_ssl = $::pulp::params::broker_use_ssl,
+  Optional[String] $tasks_login_method = $::pulp::params::tasks_login_method,
+  String $email_host = $::pulp::params::email_host,
+  Integer[1, 65535] $email_port = $::pulp::params::email_port,
+  String $email_from = $::pulp::params::email_from,
+  Boolean $email_enabled = $::pulp::params::email_enabled,
+  Boolean $manage_squid = $::pulp::params::manage_squid,
+  Optional[String] $lazy_redirect_host = $::pulp::params::lazy_redirect_host,
+  Optional[Integer[1, 65535]] $lazy_redirect_port = $::pulp::params::lazy_redirect_port,
+  Optional[String] $lazy_redirect_path = $::pulp::params::lazy_redirect_path,
+  Boolean $lazy_https_retrieval = $::pulp::params::lazy_https_retrieval,
+  Integer[0] $lazy_download_interval = $::pulp::params::lazy_download_interval,
+  Integer[0] $lazy_download_concurrency = $::pulp::params::lazy_download_concurrency,
+  Optional[Stdlib::Absolutepath] $consumers_crl = $::pulp::params::consumers_crl,
+  Boolean $reset_cache = $::pulp::params::reset_cache,
+  Enum['none', 'optional', 'require', 'optional_no_ca'] $ssl_verify_client = $::pulp::params::ssl_verify_client,
+  String $ssl_protocol = $::pulp::params::ssl_protocol,
+  Boolean $repo_auth = $::pulp::params::repo_auth,
+  Optional[String] $proxy_url = $::pulp::params::proxy_url,
+  Optional[Integer[1, 65535]] $proxy_port = $::pulp::params::proxy_port,
+  Optional[String] $proxy_username = $::pulp::params::proxy_username,
+  Optional[String] $proxy_password = $::pulp::params::proxy_password,
+  Optional[String] $yum_max_speed = $::pulp::params::yum_max_speed,
+  Integer[0] $num_workers = $::pulp::params::num_workers,
+  Boolean $enable_katello = $::pulp::params::enable_katello,
+  Boolean $enable_crane = $::pulp::params::enable_crane,
+  Optional[Integer[0]] $max_tasks_per_child = $::pulp::params::max_tasks_per_child,
+  Boolean $enable_docker = $::pulp::params::enable_docker,
+  Boolean $enable_rpm = $::pulp::params::enable_rpm,
+  Boolean $enable_puppet = $::pulp::params::enable_puppet,
+  Boolean $enable_python = $::pulp::params::enable_python,
+  Boolean $enable_ostree = $::pulp::params::enable_ostree,
+  Boolean $enable_parent_node = $::pulp::params::enable_parent_node,
+  Boolean $enable_http = $::pulp::params::enable_http,
+  Boolean $manage_broker = $::pulp::params::manage_broker,
+  Boolean $manage_db = $::pulp::params::manage_db,
+  Boolean $manage_httpd = $::pulp::params::manage_httpd,
+  Boolean $manage_plugins_httpd = $::pulp::params::manage_plugins_httpd,
+  Stdlib::Absolutepath $node_certificate = $::pulp::params::node_certificate,
+  Boolean $node_verify_ssl = $::pulp::params::node_verify_ssl,
+  Stdlib::Absolutepath $node_server_ca_cert = $::pulp::params::node_server_ca_cert,
+  String $node_oauth_effective_user = $::pulp::params::node_oauth_effective_user,
+  String $node_oauth_key = $::pulp::params::node_oauth_key,
+  String $node_oauth_secret = $::pulp::params::node_oauth_secret,
+  Array[String] $disabled_authenticators = $::pulp::params::disabled_authenticators,
+  Hash[String, String] $additional_wsgi_scripts = $::pulp::params::additional_wsgi_scripts,
+  Integer[0] $puppet_wsgi_processes = $::pulp::params::puppet_wsgi_processes,
+  Integer[0] $migrate_db_timeout = $::pulp::params::migrate_db_timeout,
+  Boolean $show_conf_diff = $::pulp::params::show_conf_diff,
+  Boolean $enable_profiling = $::pulp::params::enable_profiling,
+  Stdlib::Absolutepath $profiling_directory = $::pulp::params::profiling_directory,
+  Optional[String] $ldap_url = $::pulp::params::ldap_url,
+  Optional[String] $ldap_bind_dn = $::pulp::params::ldap_bind_dn,
+  Optional[String] $ldap_bind_password = $::pulp::params::ldap_bind_password,
+  String $ldap_remote_user_attribute = $::pulp::params::ldap_remote_user_attribute,
 ) inherits pulp::params {
-  validate_bool($enable_katello)
-  validate_bool($enable_crane)
-  validate_bool($enable_docker)
-  validate_bool($enable_rpm)
-  validate_bool($enable_puppet)
-  validate_bool($enable_python)
-  validate_bool($enable_ostree)
-  validate_bool($enable_http)
-  validate_bool($manage_db)
-  validate_bool($manage_broker)
-  validate_bool($manage_httpd)
-  validate_bool($manage_plugins_httpd)
-  validate_bool($enable_parent_node)
-  validate_bool($repo_auth)
-  validate_bool($reset_cache)
-  validate_bool($db_unsafe_autoretry)
-  validate_bool($messaging_event_notifications_enabled)
-  validate_bool($manage_squid)
-  validate_bool($lazy_https_retrieval)
-  validate_bool($show_conf_diff)
-  validate_array($disabled_authenticators)
-  validate_hash($additional_wsgi_scripts)
-  validate_integer($max_keep_alive)
-  validate_bool($enable_profiling)
-  validate_absolute_path($profiling_directory)
-
-  if $max_tasks_per_child {
-    validate_integer($max_tasks_per_child)
-  }
-
-  if $https_cert {
-    validate_absolute_path($https_cert)
-  }
-  if $https_key {
-    validate_absolute_path($https_key)
-  }
-  if $https_chain {
-    validate_absolute_path($https_chain)
-  }
-  if $ssl_protocol != undef {
-    validate_string($ssl_protocol)
-  }
-
   if $yum_max_speed {
-    validate_string($yum_max_speed)
     $real_yum_max_speed = to_bytes($yum_max_speed)
   } else {
     $real_yum_max_speed = undef
   }
 
   if $ldap_url {
-    validate_string($ldap_url)
-    validate_string($ldap_bind_dn)
-    validate_string($ldap_bind_password)
-    validate_string($ldap_remote_user_attribute)
+    assert_type(String, $ldap_url)
+    assert_type(String, $ldap_bind_dn)
+    assert_type(String, $ldap_bind_password)
   }
 
   include ::mongodb::client
