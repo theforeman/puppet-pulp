21fb2d1cfb5805692aa64ef95b1d8596f85728c9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-04-15 15:14:01 +0200

    Add repo classes

---
+++ manifests/repo/upstream.pp
@@ -0,0 +1,12 @@
+class pulp::repo::upstream(
+  $version = 2,
+) {
+  # From https://repos.fedorapeople.org/repos/pulp/pulp/rhel-pulp.repo
+  yumrepo { "pulp-${version}-stable":
+    baseurl  => "https://repos.fedorapeople.org/repos/pulp/pulp/stable/${version}/\$releasever/\$basearch/",
+    descr    => "Pulp ${version} Production Releases",
+    enabled  => true,
+    gpgcheck => true,
+    gpgkey   => "https://repos.fedorapeople.org/repos/pulp/pulp/GPG-RPM-KEY-pulp-${version}",
+  }
+}
