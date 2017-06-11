e536ab5be776ac6f4322ec899537c96b2982aca0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-06-11 14:46:44 +0300

    Add acceptance tests

---
+++ spec/acceptance/pulp_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install pulp' do
+  let(:pp) do
+    <<-EOS
+    class { '::pulp::repo::upstream': } ->
+    class { '::pulp': }
+    EOS
+  end
+
+  it_behaves_like 'a idempotent resource'
+
+  ['httpd', 'mongod', 'pulp_celerybeat', 'pulp_workers', 'pulp_resource_manager', 'pulp_streamer'].each do |service_name|
+    describe service(service_name) do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+
+  describe port(80) do
+    it { is_expected.to be_listening }
+  end
+
+  describe port(443) do
+    it { is_expected.to be_listening }
+  end
+end
